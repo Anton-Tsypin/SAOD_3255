f0028e37a480f09ce0831147a6fdac2a40f94285
Anton-Tsypin <anton.tsypin.s@gmail.com>
2022-09-19 10:56:23 +0300

    Task_1

---
+++ Task_1/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "utils.cpp"
+
+using namespace std;
+
+int main()
+{
+  int x = 3, y = 5;
+  cout << Plus(x, y) << endl;
+  return 0;
+}
---
+++ Task_1/utils.cpp
@@ -0,0 +1,3 @@
+#include "utils.h"
+
+int Plus(int a, int b) { return a + b; }
