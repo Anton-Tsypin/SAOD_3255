5226fa644c418eeb539c72a5f1979416aeaef934
Anton-Tsypin <anton.tsypin.s@gmail.com>
2022-10-15 17:44:38 +0300

    Task_6a

--- Main_tasks/Task_6a/main.cpp
+++ Main_tasks/Task_6a/main.cpp
@@ -3,9 +3,33 @@
 #include <iostream>
 #include "main.h"
 
+void copy_testing(Array a){}
+
 int main(){
-  int a[] = { 1, 2, 3, 4 };
-  int b[] = { 0, 0, 0, 0};
-  _data test = _data(4, b);
+  int a[16] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+  // int b[] = { 1, 2, 3, 4, 5, 6 };
+  // int c[] = { 0, 0, 0, 0 };
+
+  Array *test = new Array(4, 3, a);
+  std::cout << *test << std::endl;
+  std::cout << "  copy_testing() start" << std::endl;
+  copy_testing(*test);
+  std::cout << "  copy_testing() end" << '\n' << std::endl;
+
+  std::cout << "rows: " << test -> Rows() << " cols: " << test -> Cols() << std::endl;
+  std::cout << "Array[1][0]: " << test -> Item(1, 0) << std::endl;
+  std::cout << "Array[3][1]: " << test -> Item(3, 1) << std::endl;
+  // std::cout << "Array[999][999]: " << test -> Item(3, 3) << std::endl;
+
+  Array *reshaped = test -> Reshape(2, 6);
+  std::cout << *reshaped;
+
+  std::cout << std::endl << "element changed in first Array" << std::endl;
+  test -> Item(1, 1) = 10 * test -> Item(1, 1); 
+  std::cout << *test << std::endl << *reshaped;
+
+  std::cout << std::endl;
+  delete test;
+  delete reshaped;
   return 0;
 }
--- Main_tasks/Task_6a/main.h
+++ Main_tasks/Task_6a/main.h
@@ -3,36 +3,112 @@
 class Array;
 
 class _data{
+  int* pData;
+  int nCount = 1;
+
+  friend class Array;
+
+  _data(int sz, int *p = 0){
+    nCount = 1;
+    pData = new int[sz];
+    if(*p == 0){
+      std::fill(pData, pData + sz, 0);
+    }
+    else{
+      pData = p;
+    }
+    std::cout << this << " _data created: ";
+    for(int *ptr = pData; ptr - pData < sz; ptr++){
+      std::cout << *ptr << ' ';
+    } std::cout << std::endl;
+  }
+  ~_data(){ 
+    delete[] pData; 
+    std::cout << this << " _data deleted" << std::endl;
+  }
+
+  void AddRef(){ 
+    nCount++; 
+    std::cout << this << " _data AddRef count: " << nCount << std::endl;
+  }
+  void Release(){ 
+    nCount--; 
+    std::cout << this << " _data Release count: " << nCount << std::endl;
+    if(nCount == 0){ 
+      delete this; 
+    } 
+  }
+};
+
+class Array{
+  int rows, cols;
+  friend class _data;
+  _data *pDataRef;
   public:
-    int* pData;
-    int nCount;
+    Array(int m, int n, int *p = 0){
+      rows = m; cols = n;
+      pDataRef = new _data(m*n, p);
+      std::cout << this << " Array created with " << rows << " x " << cols << " shape | data: " << pDataRef << std::endl;
+    }
 
-    friend class Array;
+    Array(const Array &a){
+      pDataRef = a.pDataRef;
+      pDataRef -> AddRef();
+      std::cout << this << " Array copied from " << &a << std::endl;
+    }
 
-    _data(int sz, int *p = 0){
-      nCount = 1;
-      pData = new int[sz];
-      std::cout << pData << std::endl;
-      if(*p == 0){
-        std::fill(pData, pData + sz, 0);
+    int Rows(){ return rows; }
+    int Cols(){ return cols; }
+
+    int & Item(int i, int j){
+      if(i >= 0 && i <= rows-1 && j >= 0 && j <= cols-1){
+        return pDataRef -> pData[cols * i + j];
       }
       else{
-        pData = p;
-      }
-      std::cout << pData << std::endl;
-      for(int *ptr = pData; ptr - pData < sz; ptr++){
-        std::cout << *ptr << ' ';
+        std::cout << "i = " << i << " | rows = " << rows-1 << " | j = " << j << " | cols = " << cols-1 << std::endl;
+        throw std::out_of_range("Index out of range");
       }
     }
-    ~_data(){ delete []pData; }
 
-    void AddRef(){ 
-      nCount++; 
+    int const Item(int i, int j) const{
+      if(i >= 0 && i <= rows-1 && j >= 0 && j <= cols-1){
+        int elem = pDataRef -> pData[cols * i + j];
+        return elem;
+      }
+      else{
+        throw std::out_of_range("Index out of range");
+      }
     }
-    void Release(){ 
-      nCount--; 
-      if(nCount == 0){ 
-        delete this; 
-      } 
+
+    Array* Reshape(int m, int n){
+      if(m * n == rows * cols){
+        Array *reshaped = new Array(m, n, pDataRef -> pData);
+        return reshaped;
+      }
+      else{
+        throw std::invalid_argument("Array & View size mismatch");
+      }
+    }
+
+    ~Array(){ 
+      this -> pDataRef -> Release(); 
+      std::cout << this << " Array deleted" << std::endl;
     }
 };
+
+
+std::ostream & operator << (std::ostream &os, Array &t){
+  int rows = t.Rows();
+  int cols = t.Cols();
+  for(int i = 0; rows - i != 0; i++){
+    os << "[ ";
+    for(int j = 0; cols - j != 0; j++){
+      int num = t.Item(i, j);
+      os << num;
+      if(j != cols-1){ os << ", "; }
+    }
+    os << " ]" << std::endl;
+  }
+
+  return os;
+} 
