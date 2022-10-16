bfe1c58d2016fe29de010ff8dc36c2069f410367
Anton-Tsypin <anton.tsypin.s@gmail.com>
2022-10-16 20:51:48 +0300

    Task_2_sorting

--- Additional_tasks/Task_2_sorting/main.cpp
+++ Additional_tasks/Task_2_sorting/main.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
+#include <string>
 
 int main(int argc, char **argv)
 {
-  for(size_t i = 0; i < argc; i++)
-  {
-    std::cout << argv[i] << std::endl;
+  std::cout << std::endl;
+  
+  size_t reverse = 0;
+  if(*argv[1] == 'r'){ reverse = 1; }
+
+  const size_t N = argc - 1 - reverse;
+  double* array = new double[N];
+
+  const size_t iter = reverse + 1;
+  for(size_t i = iter; i < argc; i++){
+    array[i - iter] = std::stod(argv[i]);
   }
+  
+  for(double *p = array; p - array < N; p++){
+    std::cout << *p << ' ';
+  }
+  std::cout << std::endl << std::endl;
 
-  // a.out
-  std::cout << *argv[0] << std::endl;
+  if(reverse){ std::cout << "reverse "; } std::cout << "sorting" << std::endl;
 
+  std::cout << std::endl;
   return 0;
 };
