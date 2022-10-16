889ac624aca7133d8b7bd664054407ccf05fbeaa
Anton-Tsypin <anton.tsypin.s@gmail.com>
2022-10-17 01:08:08 +0300

    Task_2_sorting

--- Additional_tasks/Task_2_sorting/main.cpp
+++ Additional_tasks/Task_2_sorting/main.cpp
@@ -1,28 +1,30 @@
 #include <iostream>
 #include <string>
+#include "main.h"
 
-int main(int argc, char **argv)
-{
-  std::cout << std::endl;
+int main(int argc, char **argv) {
   
-  size_t reverse = 0;
-  if(*argv[1] == 'r'){ reverse = 1; }
+  std::cout << std::endl;
+  bool reverse = false;
+  if (*argv[1] == 'r') { reverse = true; }
 
   const size_t N = argc - 1 - reverse;
   double* array = new double[N];
+  double* sorted_array = new double[N];
 
   const size_t iter = reverse + 1;
-  for(size_t i = iter; i < argc; i++){
+  for (size_t i = iter; i < argc; i++) {
     array[i - iter] = std::stod(argv[i]);
   }
   
-  for(double *p = array; p - array < N; p++){
-    std::cout << *p << ' ';
-  }
-  std::cout << std::endl << std::endl;
+  Print_array(array, N);
 
-  if(reverse){ std::cout << "reverse "; } std::cout << "sorting" << std::endl;
+  if (reverse) { std::cout << "reverse "; } std::cout << "sorting" << std::endl;
 
+  Sort_array(array, N, reverse);
+
+  Print_array(array, N);
+  
   std::cout << std::endl;
   return 0;
 };
--- Additional_tasks/Task_2_sorting/main.h
+++ Additional_tasks/Task_2_sorting/main.h
@@ -1,2 +1,36 @@
 #include <iostream>
 
+void Print_array(double* array, size_t N) {
+  std::cout << '[';
+  for (double *p = array; p - array < N; p++) {
+    std::cout << *p; 
+    if (p - array == N - 1) { continue; }
+    std::cout << ", ";
+  }
+  std::cout << ']' << std::endl;
+}
+
+bool Condition(double first, double second, bool reverse) {
+  if(reverse) { return first < second; }
+  else { return first > second; }
+}
+
+void Sort_array(double* array, size_t N, bool reverse) {
+
+  int flag_break;
+  double temp;
+
+  for (size_t i = 0; i < N - 1; i++) {
+    flag_break = 1;
+    for (size_t j = 0; j < N - i - 1; j++) {
+
+      if (Condition(array[j], array[j + 1], reverse)) {
+        temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
+        flag_break = 0;
+      }
+    }
+    if (flag_break) { break; }
+  }
+}
