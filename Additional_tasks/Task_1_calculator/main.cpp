cabe092296597322e3b0887ddf6646ccd4440c87
Anton-Tsypin <anton.tsypin.s@gmail.com>
2022-10-16 23:17:51 +0300

    Task_1_calculator

--- Additional_tasks/Task_1_calculator/main.cpp
+++ Additional_tasks/Task_1_calculator/main.cpp
@@ -3,11 +3,13 @@
 
 int main(int argc, char **argv){
 
-  double a, b, result; char c;
+  double a = 0, b = 0, result = 0; char c = '+';
   
-  a = std::stod(argv[1]);
-  c = *argv[2];
-  b = std::stod(argv[3]);
+  if(argc > 3){
+    a = std::stod(argv[1]);
+    c = *argv[2];
+    b = std::stod(argv[3]);
+  }
 
   switch(c){
     case '+': result = a + b; break;
@@ -16,7 +18,7 @@ int main(int argc, char **argv){
     case '/': result = a / b; break;
   }
 
-  std::cout << "Result is: " << result << std::endl;
+  std::cout << a << ' ' << c << ' ' << b << " = " << result << std::endl;
 
   return 0;
 }
