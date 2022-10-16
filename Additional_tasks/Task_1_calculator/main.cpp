caedf42e9502906f5ced5758c5c70427e84357ea
Anton-Tsypin <anton.tsypin.s@gmail.com>
2022-10-16 22:46:12 +0300

    Task_1_calculator

--- Additional_tasks/Task_1_calculator/main.cpp
+++ Additional_tasks/Task_1_calculator/main.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
+#include <string>
 
-int main(){
+int main(int argc, char **argv){
 
   double a, b, result; char c;
+  
+  a = std::stod(argv[1]);
+  c = *argv[2];
+  b = std::stod(argv[3]);
 
-  while(1){
-    std::cin >> a >> c >> b;
-    switch(c){
-      case '+': result = a + b; break;
-      case '-': result = a - b; break;
-      case '*': result = a * b; break;
-      case '/': result = a / b; break;
-    }
-    std::cout << "Result is: " << result << std::endl;
+  switch(c){
+    case '+': result = a + b; break;
+    case '-': result = a - b; break;
+    case '*': result = a * b; break;
+    case '/': result = a / b; break;
   }
+
+  std::cout << "Result is: " << result << std::endl;
 
   return 0;
 }
