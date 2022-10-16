9bb3a4ccc92b7acd1139a8181f846f375e195131
Anton-Tsypin <anton.tsypin.s@gmail.com>
2022-10-16 04:44:48 +0300

    addition_task_1

--- Additional_tasks/Task_1_calculator/main.cpp
+++ Additional_tasks/Task_1_calculator/main.cpp
@@ -1,1 +1,19 @@
 #include <iostream>
+
+int main(){
+
+  double a, b, result; char c;
+
+  while(1){
+    std::cin >> a >> c >> b;
+    switch(c){
+      case '+': result = a + b; break;
+      case '-': result = a - b; break;
+      case '*': result = a * b; break;
+      case '/': result = a / b; break;
+    }
+    std::cout << "Result is: " << result << std::endl;
+  }
+
+  return 0;
+}
