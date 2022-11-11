8c63ebd1025b1d0c60ab9e401abb29454d3c11c0
Anton-Tsypin <anton.tsypin.s@gmail.com>
2022-11-11 17:35:03 +0300

    task_6a fix

--- Main_tasks/Task_6a/main.cpp
+++ Main_tasks/Task_6a/main.cpp
@@ -7,8 +7,6 @@ void copy_testing(Array a){}
 
 int main(){
   int a[16] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
-  // int b[] = { 1, 2, 3, 4, 5, 6 };
-  // int c[] = { 0, 0, 0, 0 };
 
   Array *test = new Array(4, 3, a);
   std::cout << *test << std::endl;
@@ -27,15 +25,17 @@ int main(){
   std::cout << *reshaped;
 
   std::cout << std::endl << "element changed in first Array" << std::endl;
-  test -> Item(1, 1) = 10 * test -> Item(1, 1); 
-  std::cout << *test << std::endl << *reshaped;
-
-  std::cout << std::endl;
+  test -> Item(1, 1) = 10 * test -> Item(1, 1);
+  std::cout << *test << std::endl << *reshaped << std::endl;
 
   // std::invalid_argument
   // Array *error_reshaped = test -> Reshape(999, 999);
+
   
-  delete test;
-  delete reshaped;
+  // почему-то без удаления test и reshaped не выдаёт ошибку об утечке памяти
+
+  // delete test;
+  // delete reshaped;
+  
   return 0;
 }
