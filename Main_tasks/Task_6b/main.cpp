4ad589e9875a986c9919eb7deaade04771d76e55
Anton-Tsypin <anton.tsypin.s@gmail.com>
2022-11-14 06:50:23 +0300

    Main_task_6b

--- Main_tasks/Task_6b/main.cpp
+++ Main_tasks/Task_6b/main.cpp
@@ -1,7 +1,49 @@
 #include <iostream>
+#include "main.h"
 
 int main(){
 
+  // Тесты хешей и сдвигов
+
+  PH* test = new PH(5);
+  size_t hash1 = test -> Hash("abcde");
+  size_t hash2 = test -> Hash("bcdef");
+  size_t shifted_hash = test -> Shift(hash1, 'a', 'f');
+  if(hash2 == shifted_hash){ std::cout << "  YES";} else { std::cout << "  NO"; } std::cout << std::endl;
+  delete test;
+
+  std::cout << std::endl;
+
+  PH* test2 = new PH(7);
+  size_t hash3 = test2 -> Hash("aabbccd");
+  size_t hash4 = test2 -> Hash("abbccdd");
+  size_t shifted_hash2 = test2 -> Shift(hash3, 'a', 'd');
+  if(hash4 == shifted_hash2){ std::cout << "  YES";} else { std::cout << "  NO"; } std::cout << std::endl;
+  delete test2;
+
+  std::cout << std::endl;
+
+  PH* test3 = new PH(20);
+  size_t hash5 = test3 -> Hash("GTAGTGTGTCTACGTCTTTA");
+  size_t hash6 = test3 -> Hash("TAGTGTGTCTACGTCTTTAT");
+  size_t shifted_hash3 = test3 -> Shift(hash5, 'G', 'T');
+  if(hash6 == shifted_hash3){ std::cout << "  YES";} else { std::cout << "  NO"; } std::cout << std::endl;
+  delete test3;
+
+  std::cout << std::endl;
+
+
+  // Тест поиска в тексте с несколькими хешами
+
+  char text[] = "abbbaaababaaabab";
+  const char* substrs[] = {"bbb", "aaa", "bab", "aba"};
+  std::cout << "text: ";
+  for(int i=0; i<strlen(text); i++){ std::cout << text[i]; } std::cout << std::endl << "substrings: ";
+  for(size_t j=0; j<4; j++){
+    for(int i=0; i<strlen(substrs[j]); i++){ std::cout << substrs[j][i]; } std::cout << " | ";
+  } std::cout << std::endl;
+  
+  find_in_text(text, substrs, 4);
 
   return 0;
 }
--- Main_tasks/Task_6b/main.h
+++ Main_tasks/Task_6b/main.h
@@ -1,2 +1,88 @@
 #include <iostream>
+#include <cstring>
+#include <cmath>
 
+class PH{
+  const unsigned short k = 2;
+  const unsigned short Q = 65521;
+
+  public:
+    size_t len;
+    unsigned long long* kp;
+
+    PH(size_t L){
+      std::cout << "PH created: ";
+      len = L+1;
+      kp = new unsigned long long[len];
+      for(size_t i=0; i<len; i++){
+        unsigned long long n = pow(k, i);
+        kp[i] = n % Q;
+        std::cout << kp[i] << " ";
+      }
+      std::cout << std::endl;
+    }
+
+    ~PH(){
+      std::cout << "PH deleted" << std::endl;
+      delete[] kp;
+    }
+
+    size_t Hash(const char* s){
+      size_t hash = 0;
+      size_t num;
+
+      for(size_t i=0; i<len-1; i++){
+        num = s[i];
+        hash += num * kp[len-2-i];
+        // std::cout << std::endl << num << " " << len-1-i << std::endl;
+      }
+      hash = hash % Q;
+
+      std::cout << "Created hash = " << hash << " for string "; 
+      for(size_t i=0; i<len-1; i++){ std::cout << s[i]; }; 
+      std::cout << std::endl;  
+
+      return hash;
+    }
+
+    size_t Shift(size_t h, char pref, char suff){
+      size_t num1 = pref;
+      size_t num2 = suff;
+
+      size_t hash = ((k * h) - (num1 * kp[len-1]) + num2) % Q;
+
+      std::cout << "Shifted hash " << h << " to " << hash; // << std::endl;  
+
+      return hash;
+    }
+};
+
+void find_in_text(char* text, const char** substrs, size_t substrs_count){
+  size_t text_len = strlen(text);
+  size_t substr_len = strlen(substrs[0]);
+  PH* pol_hash = new PH(substr_len);
+  size_t hashes[5]; 
+  
+  for(size_t i=0; i<substrs_count; i++){
+    hashes[i] = pol_hash -> Hash(substrs[i]);
+  } std::cout << std::endl;
+
+  size_t current_hash;
+  char* current_substr = new char[substr_len+1];
+  
+  for(size_t i=0; i<(text_len-substr_len)+1; i++){
+    strncpy(current_substr, &text[i], substr_len); current_substr[substr_len] = 0;
+    current_hash = pol_hash -> Hash(current_substr);
+
+    for(size_t j=0; j<substrs_count; j++){
+      bool flag = false;
+      if(current_hash == hashes[j]){
+        if(strcmp(current_substr, substrs[j]) == 0){
+          std::cout << "substring " << substrs[j] << " success with index " << i << std::endl << std::endl;
+        }
+      }
+    }
+  }
+
+  delete pol_hash;
+}
